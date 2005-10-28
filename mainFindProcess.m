3afe7dcc2bebea1ea29695fb3dc49c64499624e6
Tetsuro Kurita <tkurita@mac.com>
2005-10-28 17:33:52 +0000

    テストツール findProcess の main()

---
+++ mainFindProcess.m
@@ -0,0 +1,69 @@
+#import "SmartActivate.h"
+#include <unistd.h>
+
+#define useLog 0
+
+void usage() {
+	printf("Usage: findProcess [-t] [creator type] [-i] [bundle identifier] [process name]\n");
+	exit(-1);
+}
+
+extern char *optarg;
+extern int optind, opterr, optopt;
+
+int main (int argc, char * const argv[]) {
+	if (argc > 6) {
+		// too many arguments
+		printf("Too many arguments.\n");
+		usage();
+	}
+	
+#if useLog
+	printf("argc %i\n",argc);
+	for (int i=0; i < argc; i++) {
+		printf("%i\t%s\n",i,argv[i]);
+	}
+#endif
+	
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+	
+	/* get arguments */
+	NSString *targetCreator = nil;
+	NSString *targetName = nil;
+	NSString *targetIdentifier = nil;
+	
+	while(getopt(argc, argv, "t:i:") != -1 ){
+		switch(optopt){
+			case 't': 
+				targetCreator = [NSString stringWithCString:optarg];
+				break;
+			case 'i': 
+				targetIdentifier = [NSString stringWithCString:optarg];
+				break;				
+			case '?':
+			default	:
+				printf("There is unknown option.\n");
+				usage(); break;
+		}
+		optarg	=	NULL;
+	}
+	
+#if useLog
+	printf("%i\t%i\n",argc,optind);
+#endif
+	
+	if (optind < argc) {		
+		targetName = [NSString stringWithCString:argv[optind]];
+	}
+#if useLog
+	NSLog(targetCreator);
+	NSLog(targetName);
+#endif
+	
+	NSDictionary *pDict = getProcessInfo(targetCreator, targetName, targetIdentifier);
+	NSLog([pDict description]);
+	
+    [pool release];
+
+	return 0;
+}
