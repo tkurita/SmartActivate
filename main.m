f01bb6bc07c91a7358ad5ecbf67c14a6f9aad8e1
Tetsuro Kurita <tkurita@mac.com>
2005-12-18 06:25:47 +0000

    GPLのコメントを追加

--- main.m
+++ main.m
@@ -1,3 +1,20 @@
+//activate -- activate an application process with bringing only one window to frontmost.
+//Copyright (C) 2005  Tetsuro KURITA <tkurita@mac.com>
+//
+//This program is free software; you can redistribute it and/or
+//modify it under the terms of the GNU General Public License
+//as published by the Free Software Foundation; either version 2
+//of the License, or (at your option) any later version.
+//
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+//
+//You should have received a copy of the GNU General Public License
+//along with this program; if not, write to the Free Software
+//Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+
 #import "SmartActivate.h"
 #include <getopt.h>
 
@@ -21,18 +38,7 @@ void showVersion() {
 	printf("activate 1.0 copyright 2005, Tetsuro KURITA\n");
 }
 
-//extern char *optarg;
-//extern int optind, opterr, optopt;
-
 int main (int argc, char * const argv[]) {
-	/*
-	if (argc > 10) {
-		// too many arguments
-		printf("Too many arguments.\n");
-		usage();
-		exit(-1);
-	}
-	 */
 
 #if useLog
 	printf("argc %i\n",argc);
