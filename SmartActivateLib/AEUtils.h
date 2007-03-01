3fe83fd0b45b5b8939490c59d752c46004f66d5b
Tetsuro Kurita <tkurita@mac.com>
2007-03-01 08:40:19 +0000

    [SmartActivateLib] AEUtils は独立したproject にする

--- SmartActivateLib/AEUtils.h
+++
@@ -1,6 +0,0 @@
-#include <Carbon/Carbon.h>
-
-OSErr getStringValue(const AppleEvent *ev, AEKeyword theKey, CFStringRef *outStr);
-void showAEDesc(const AppleEvent *ev);
-void safeRelease(CFTypeRef theObj);
-OSErr putBoolToReply(Boolean aBool, AppleEvent *reply);
